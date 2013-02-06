8f56ad9dc2d0eaa455d114ee7b462752595218fd
David Snelling <snellind@os-class.engr.oregonstate.edu>
2013-02-05 18:34:02 -0800

    File verification added, as well as dome beginning code to append() 6:35pm 2/5/2013

--- myar.c
+++ myar.c
@@ -1,35 +1,71 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <ar.h>
+#include <fcntl.h>
 
-void user_error() // fix entire
+void error(char *message)
 {
-	printf("\nException: User error encountered\n\n");
+	printf("\nExiting process: %s\n\n", message);
 	exit(1);
 }
 
-void archive_error() // fix entire
+void user_error()
 {
-	printf("\nException: Archive error encountered\n\n");
-	exit(1);
+	error("User error encountered");
 }
 
-void file_error() // fix entire
+void archive_error()
 {
-	printf("\nException: File error encountered\n\n");
-	exit(1);
+	error("Archive error encountered");
 }
 
-void program_error() // fix entire
+void file_error()
 {
-	printf("\nException: Program error encountered\n\n");
-	exit(1);
+	error("File not found error encountered");
+}
+
+void program_error()
+{
+	error("Program error encountered");
 }
 
 int is_archive(char *afile) // fix entire
 {
-	printf("Archive being verified...\n");
-	return 0;
+	int success = 1;
+	if(!is_file(&afile, 1)) {
+		printf("Exception: Archive file does not exist\n");
+		success = 0;
+	} else {
+		int file_desc = open(afile, O_RDONLY);
+		if(file_desc != -1) {
+			char buffer[SARMAG + 1]; // Used to read archive identifier
+			read(file_desc, buffer, SARMAG);
+			buffer[SARMAG] = '\0'; // Appending a string termination
+			if(strcmp(buffer, ARMAG) != 0) { // Archive identifier mismatch
+				printf("Exception: Archive format invalid");
+				archive_error();
+			}
+		} else {
+			printf("Exception: Cannot open archive\n");
+			archive_error();
+		}
+	}
+	return success;
+}
+
+int is_file(char **files, int file_count)
+{
+	int success = 1;
+	int i;
+	for(i = 0; i < file_count; i++) {
+		if(access(files[i], F_OK) == -1) {
+			success = 0;
+			printf("Exception: %s not found\n", files[i]);
+		}
+	}
+	return success;
 }
 
 void concise_table(char *afile) // fix entire
@@ -53,8 +89,20 @@ char ** fetch_files_cd(int *file_count) // fix entire
 	return files;
 }
 
+int create_archive(char *afile) // fix entire
+{
+	int created = 1;
+	printf("Creating archive: %s\n", afile);
+	return created;
+}
+
 void append(char *afile, char **files, int file_count) // fix entire
 {
+	if(!is_archive(afile))
+		if(!create_archive(afile)) {
+		   printf("Exception: Archive not created");
+		   archive_error();
+		}
 	int c;
 	for(c = 0; c < file_count; c++)
 		printf("%s appended to %s\n%d files appended\n", files[c], afile, c + 1);
@@ -121,6 +169,8 @@ int main(int argc, char **argv)
 		int file_count = argc - 3; // Number of files = argc - 3
 		char **files = get_files(file_count, argv);
 		if(key[1] == 'q') // If key is 'q'
+			if(!is_file(files, file_count)) // DELETE ME
+				file_error(); // DELETE ME
 			append(afile, files, file_count);
 		if(!is_archive(afile)) // Verifies archive exists
 			archive_error();
